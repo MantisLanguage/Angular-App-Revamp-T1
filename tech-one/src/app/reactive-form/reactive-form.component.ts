f44fb471796163b8875dbc895f51efff8e42e2fa
Connor Dougherty <connor.j.dougherty9@gmail.com>
2020-01-28 01:19:05 -0700

    reactive form not working, but stashing changes for progression so far

---
+++ tech-one/src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent {
+  name = new FormControl('');
+}
